e3f2681927fea121713b460ff877cccb8ffd6462
lrtipple <ltipple23@att.net>
2022-05-14 14:42:05 -0400

    adding mapping_lines code logic

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,12 +1,38 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([30.1944, -97.6700], 5);
 //  Add a marker to the map for Los Angeles, California.
 //let marker = L.marker([34.0522, -118.2437].addTo(map));
 
+// Coordinates for each point to be used in the line.
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790],
+//     [40.7899, -111.9791],
+//     [47.4502, -122.3088]
+//   ];
+
+//bonus skill create airline route from SFO to JFK, three stops AUS CMH and YYZ
+let line = [
+    [37.6213, -122.3790],
+    [30.1944, -97.6700],
+    [39.995, -82.8891],
+    [43.6775, -79.6308],
+    [40.641766, -73.7809]
+  ];
+// Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//     color: "red"
+//   }).addTo(map);
+
+// Create a polyline using the line coordinates and make the line yellow.
+L.polyline(line, {
+    color: "yellow"
+  }).addTo(map);
+
 // Get data from cities.js
-let cityData = cities;
+// let cityData = cities;
 
   
 // Loop through the cities array and create one marker for each city.
@@ -19,19 +45,19 @@ let cityData = cities;
     //L.marker(city.location).addTo(map);
 // });
 
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/80000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         radius: city.population/80000
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// });
 
 // An array containing each city's location, state, and population.
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
